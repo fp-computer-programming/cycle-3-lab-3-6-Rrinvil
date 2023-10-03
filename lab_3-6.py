63f25f2bc3850471339b690b533d23da2d858422
Rrinvil <144148135+Rrinvil@users.noreply.github.com>
2023-10-03 08:54:23 -0400

    Add files via upload

---
+++ lab_3-6.py
@@ -0,0 +1,23 @@
+
+"""Write a statement that creates the string "Fairfield Prep" by concatenating substrings.
+
+Write a statement that creates a dashed line that is 20 dashes long.
+
+Write a statement that prints the following:
+I'm loving this short story I'm reading, "The Fall of the House of Usher"
+
+Write a statement that prints the length of the previous string.
+
+Write a statement using the index operator that returns the "L" in "apple"
+
+"""
+x = 'fairfield' +  ''+ 'prep'
+print(x)
+x = "-" * 20
+print(x)
+x = "I'm loving this short story I'm reading, ""the fall of the house of usher"
+print(x)
+x = "-" * 70
+print(x)
+x= "apple"
+print(x[3])
